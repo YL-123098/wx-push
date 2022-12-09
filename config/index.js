1ff589b63f6b034ca16bee3f91cc2c9d8b718198
YL-123098 <120152773+YL-123098@users.noreply.github.com>
2022-12-09 11:15:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxa44a9b2ead753c5b",
+  appsecret: "7f74f7637979758df59bc96f8db9aa98",
+  template_id: "AoMXibiuqjLfQ3K2mrsq7fbEtha082GNpl-lSLJ3DhE",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "德阳",
+  first_date: "2022-06-09",
 };
